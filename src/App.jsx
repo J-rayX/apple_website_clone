64ab1d91cf10bf5bc3f7f3069c15377247785c04
jkaylight <jkaylight@gmail.com>
2024-06-18 14:04:37 +0100

    3D model section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
+import * as Sentry from '@sentry/react'
 
 const App = () => {
 
@@ -9,8 +11,9 @@ const App = () => {
       <Navbar /> 
       <Hero /> 
       <Highlights /> 
+      <Model />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
